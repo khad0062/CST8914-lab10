a04fd4b442eaba5c86469dc347f2fb5e3a8d5566
Nirajan Khadka <khad0062@algonquinlive.com>
2025-03-14 22:11:27 -0400

    Update JS.js

--- JS.js
+++ JS.js
@@ -61,7 +61,13 @@ class MenuButtonActions {
 
   setFocusToMenuitem(newMenuitem) {
     this.menuitemNodes.forEach(function (item) {
-// TOUFIC'S COMMENT: Placeholder for the roving tabindex logic  ;)
+// TOUFIC'S COMMENT: Placeholder for the roving tabindex logic  item.
+      tabIndex = -1;
+    });
+    if (newMenuitem) {
+        newMenuitem.tabIndex = 0;
+        newMenuitem.focus();
+      
     });
   }
 
